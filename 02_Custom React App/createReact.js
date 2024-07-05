1ae3ec43c338f7f93f8b3ec685e36580cfc0b8ef
Hitesh <hiteshghanchi87@gmail.com>
2024-07-05 17:34:16 +0530

    Custom react basic

--- 02_Custom React App/createReact.js
+++ 02_Custom React App/createReact.js
@@ -1,5 +1,17 @@
 // remember if we export then always make jsx start from Capital Letter
+// react tree has attribute and children 
 
+function customRender(container,object){
+    const domElement = document.createElement(object.type);
+    domElement.innerHTML = object.children;
+    
+    for (const prop in object.props) {
+        if(prop==="children") continue;
+       domElement.setAttribute(prop,object.props[prop]);
+    }
+
+    container.appendChild(domElement)
+}
 
 //this is how react stores the element as, remember their keys name may differ
 const oneELement = {
